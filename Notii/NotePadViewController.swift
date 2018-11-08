ff8b62e9d5c8ea85f56c0c8e208473f1f9ec1a35
asge <asmow@yahoo.com>
2018-11-07 19:41:37 -0600

    Note list complete with dataSource and Delegate methods.

---
+++ Notii/NotePadViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  Notii
+//
+//  Created by Asgedom Teklu on 11/7/18.
+//  Copyright © 2018 Asgedom Y. All rights reserved.
+//
+
+import UIKit
+
+class NotePadViewController: UITableViewController {
+
+    let noteArray = ["fatal error: Index out of range",
+                    "How do I add a header based on API information in Javascript"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    //MARK: - TableView DataSource Methods
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return noteArray.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "NotPadCell", for: indexPath)
+        cell.textLabel?.text = noteArray[indexPath.row]
+        return cell
+    }
+    
+    //Mark: - TableView Delegete Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        //        context.delete(itemArray[indexPath.row])
+        //        itemArray.remove(at: indexPath.row)
+        //        itemArray[indexPath.row].setValue("Completed", forKey: "title")
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    
+    }
+    
+}
+
+
